f15d6778b27d7c1b3a4ad4a902d080e85667689d
Abhishek Singh <abhishekfromup@gmail.com>
2018-07-26 17:37:12 +0530

    added function_pointers_in_c.c to understand the concept of pointers to function

---
+++ function_pointers_in_c.c
@@ -0,0 +1,21 @@
+// function_pointers_in_c.c
+
+#include <stdio.h>
+
+float sum(float, float);
+
+float sum(float m, float n)
+{
+	return (m+n);
+}
+
+
+int main()
+{
+	float (*fun_ptr)(float, float);
+	fun_ptr = sum;
+
+	printf("%f, %f\n", sum(3,4), fun_ptr(4,5));
+
+	return 0;
+}
